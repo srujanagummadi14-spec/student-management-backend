ab5d0e8eba08bd59ba99d48d9a791a7567d9785b
srujanagummadi14-spec <srujanagummadi14@gmail.com>
2026-06-20 13:56:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -64,7 +64,9 @@ app.delete("/students/:id", (req, res) => {
   });
 });
 
-// Start Server
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+// Start Server (Render Compatible)
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
